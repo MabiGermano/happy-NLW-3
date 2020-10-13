1a3f2b14b7bebf7d50382cb92a8503658bef2860
MabiGermano <mabi.germano.e@gmail.com>
2020-10-13 01:27:00 -0300

    landingpage e rotas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
-
 import './styles/global.css';
-import './styles/pages/landing.css'
+import Routes from './routes';
 
-import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy logo" />
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-        <div className="location">
-          <strong>Recife</strong>
-          <span>Pernambuco</span>
-        </div>
-
-        <a href="" className="enter-app">
-          >
-        </a>
-      </div>
-    </div>
+    <Routes/>
   );
 }
 
