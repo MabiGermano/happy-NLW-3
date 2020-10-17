833c4d60f7745d9132aa695aae7403b7c0ba607a
MabiGermano <mabi.germano.e@gmail.com>
2020-10-17 14:50:44 -0300

    init

---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+// Rotas 
+// Rescursos: User nesse caso
+
+//Query params: http://localhost:3333/users?search=texto&page=2
+// Route param: http://localhost:3333/users/3 (identificador do recurso)
+// Body
+
+app.get('/users', (request, response) => {
+    console.log(request.query)
+    console.log(request.params)
+    return response.json({message: "Hello world"});
+})
+
+
+app.listen(3333);
+
