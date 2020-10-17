2d32a5b769e2879304a0fb3e0b1b266a8feac48b
MabiGermano <mabi.germano.e@gmail.com>
2020-10-17 18:45:31 -0300

    Configurações de banco

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,11 @@
 import express from 'express';
+import './database/connection';
 
 const app = express();
 app.use(express.json())
 
-// Rotas 
-// Rescursos: User nesse caso
-
-//Query params: http://localhost:3333/users?search=texto&page=2
-// Route param: http://localhost:3333/users/3 (identificador do recurso)
-// Body
-
 app.get('/users:id', (request, response) => {
-    console.log(request.query)
-    console.log(request.params)
-    console.log(request.body)
+    
     return response.json({message: "Hello world"});
 })
 
